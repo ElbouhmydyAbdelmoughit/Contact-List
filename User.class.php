16ab6d19940e90115b1e0157245da913185a1089
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-04-12 11:21:05 +0000

    add geter and seter in class user

--- User.class.php
+++ User.class.php
@@ -3,9 +3,11 @@ class User{
     public $username;
     private $password_user;
     
-    public function __construct($username,$password_user)
+    public function segtPassword($password_user)
     {
-        $this->username=$username;
         $this->password_user=$password_user;
+    }
+    public function getPassword(){
+        return $this->password_user;
     }
 }
