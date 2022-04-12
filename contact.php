177ea2eb2ea06c19361ea70982f89a44850faa66
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-04-12 23:20:43 +0000

    define geter and seter name

--- contact.php
+++ contact.php
@@ -1,53 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/Normalize.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2">
-    <title>Contact List</title>
-</head>
-<body>
-    <!---Create navbar--->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="NAVBAR_HOME_PAGE">
-        <div class="container">
-            <!---Logo navbar-->
-            <a class="navbar-brand" href="index.php">
-                <img src="assets/img/Contact_logo_copy-removebg-preview.png" id="Logo_page" alt="image logo">
-            </a>
-            <!----Button menu--->
-            <button 
-                    class="navbar-toggler" 
-                    type="button" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#navmenu"
-                    aria-controls="navmenu" 
-                    aria-expanded="false" 
-                    aria-label="Toggle navigation" 
-                    style="background-color: #FF8303;">
-                    <!-------icon menu--->
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!----List navbar--->
-            <div class="collapse navbar-collapse" id="navmenu">
-                <ul class="navbar-nav ms-auto">
-                    <!----navb Button Login-->
-                    <li class="nav-item">
-                        <a href="#" type="button" class="btn" id="btn_contact" data-bs-toggle="modal" data-bs-target="#exampleModal">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="btn" id="btn_logout"> logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!------Header------>
-    <?php include("Add_contact.php")?>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
+<?php
+class Contact{
+    /*properties*/
+   private $name;
+   private $email;
+   private $address;
+   private $phone;
+   /*Methods*/
+
+   public function __construct($name,$email,$address,$phone){
+       $this->name = $name; 
+       $this->email = $email; 
+       $this->address = $address; 
+       $this->phone = $phone; 
+   }
+   public function setName($name){
+       $this->name=$name;
+   }
+   public function getName($name){
+    return $name;
+}
+}
