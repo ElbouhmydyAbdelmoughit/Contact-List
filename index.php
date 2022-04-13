228ae9f4c73d2a5f4ec1064ad78bb5d6fa6dc90a
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-04-13 09:34:58 +0000

    added animation

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
     <title>Contact List</title>
 </head>
 <body>
@@ -16,7 +16,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light" id="NAVBAR_HOME_PAGE">
         <div class="container">
             <!---Logo navbar-->
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand animate__animated animate__slideInUp" id="logo_animate" href="index.php">
                 <img src="assets/img/Contact_logo_copy-removebg-preview.png" id="Logo_page" alt="image logo">
             </a>
             <!----Button menu--->
@@ -36,7 +36,7 @@
             <div class="collapse navbar-collapse" id="navmenu">
                 <ul class="navbar-nav ms-auto">
                     <!----navb Button Login-->
-                    <li class="nav-item">
+                    <li class="nav-item animate__animated animate__slideInUp" id="btn_login_animate">
                         <a href="Login.php" class="nav-link col-3" id="login_text">Login</a>
                     </li>
                 </ul>
@@ -45,21 +45,23 @@
     </nav>
     <!------Header------>
     <header>
-        <div class="container animate__animated animate__bounce">
+        <div class="container">
             <div class="d-lg-flex">
                 <!-----Text header----->
-                <div>
+                <div class="animate__animated animate__slideInUp" id="h1_animate">
                     <h1 class="tetx-wrap">
                         <span>Hello!</span>
                         sign up to start creating your contacts list.Already have an account? Login here.
                     </h1>
                 </div>
                 <!--------image header------->
-                <img src="assets/img/picture_home_page.svg" class="img-fluid w-50" alt="picture session">
+                <img src="assets/img/picture_home_page.svg" id="image_section_animate" class="img-fluid w-50 animate__animated animate__slideInUp" alt="picture session">
             </div>
-            <a href="Login.php">
-                <button class="btn my-4" id="button_login" class="w-sm-25 btn_continue">Continue</button>
-            </a>
+            <div class="animate__animated animate__slideInUp" id="btn_continue_animate">
+                <a href="Login.php">
+                    <button class="btn my-4" id="button_login" class="w-sm-25 btn_continue">Continue</button>
+                </a>
+            </div>
         </div>
     </header>
 
