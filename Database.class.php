d461eece6a6375970e45cc286eedd532e1f78a73
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2022-04-16 21:04:36 +0000

    update code function query

--- Database.class.php
+++ Database.class.php
@@ -8,12 +8,18 @@ class Db{
         
     public static function Connect(){
         try {
-            self::$connection = new PDO("mysql:host=".self::$host.";dbname=".self::$dbname."",self::$user,self::$password);
+            self::$connection = new PDO('mysql:host=".self::$host.";dbname=".self::$dbname"',self::$user,self::$password);
         } catch (PDOException $e) {
             die($e->getMessage());
         }
         return self::$connection;
     }
+
+    public static function query($requete){
+         $db = self::Connect();
+        $sql=$db->query($requete);
+    }
+
     public static function deconnect(){
         self::$connection =NULL; 
     }
